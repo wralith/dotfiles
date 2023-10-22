bd61ec7b8338fa1b8e56a44eb2207b603a386d4b
Wralith <wralithdev@gmail.com>
2023-10-22 13:54:14 +0300

    use official copilot plugin

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,7 +2,7 @@ lvim.log.level = "warn"
 lvim.builtin.lualine.sections.lualine_a = { "mode" }
 lvim.builtin.alpha.active = true
 lvim.builtin.illuminate.active = true
-lvim.use_icons = false
+lvim.use_icons = true
 
 lvim.builtin.indentlines.active = false
 
@@ -20,15 +20,14 @@ vim.opt.foldlevelstart = 90
 
 lvim.builtin.treesitter.highlight.enable = true
 
-local luasnip = require("luasnip")
-local cmp = require("cmp")
-
 -- plugins
 lvim.plugins = {
     -- Colorschemes
     { "shaunsingh/nord.nvim" },
-    { "catppuccin/nvim",            name = "catpuccin" },
-    { "rose-pine/neovim",           name = "rose-pine" },
+    { "catppuccin/nvim",                name = "catpuccin" },
+    { "rose-pine/neovim",               name = "rose-pine" },
+    { "olivercederborg/poimandres.nvim" },
+    { "folke/tokyonight.nvim",          lazy = "false" },
 
     -- Visuals
     { "folke/todo-comments.nvim" },
@@ -37,6 +36,7 @@ lvim.plugins = {
 
     -- Lsp realted stuff
     { "fatih/vim-go" },
+    { "simrat39/rust-tools.nvim" },
     {
         "ray-x/lsp_signature.nvim",
         event = "BufRead",
@@ -61,8 +61,6 @@ lvim.plugins = {
             vim.api.nvim_set_keymap("n", "S", ":HopPattern<CR>", {})
         end
     },
-
-    -- AI
     {
         "github/copilot.vim",
         event = "VeryLazy",
@@ -70,30 +68,10 @@ lvim.plugins = {
             -- copilot assume mapped
             vim.g.copilot_assume_mapped = true
             vim.g.copilot_no_tab_map = true
-            vim.g.copilot_tab_fallback = ""
-        end,
-    },
-    {
-        "hrsh7th/cmp-copilot",
-        config = function()
-lvim.builtin.cmp.mapping["Tab"] = cmp.mapping(function(fallback)
-    local copilot_keys = vim.fn['copilot#Accept']()
-    if cmp.visible() then
-        cmp.select_next_item()
-    elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-    elseif copilot_keys ~= '' and type(copilot_keys) == 'string' then
-        vim.api.nvim_feedkeys(copilot_keys, 'i', true)
-    else
-        fallback()
-    end
-end, {
-    'i',
-    's',
-})
         end,
     },
 }
+
 
 -- formatters and linters
 local linters = require("lvim.lsp.null-ls.linters")
@@ -104,20 +82,42 @@ linters.setup {
     { command = "eslint_d", filetypes = { "typescript", "typescriptreact", "js", "jsx" } }
 }
 formatters.setup {
-    { command = "prettierd", filetypes = { "typescript", "typescriptreact", "js", "jsx" } }
+    { command = "prettierd", filetypes = { "typescript", "typescriptreact", "js", "jsx", "html" } }
+}
+formatters.setup {
+    { command = "shfmt", filetypes = { "sh" } }
 }
 code_actions.setup {
     { command = "eslint_d", filetypes = { "typescript", "typescriptreact", "js", "jsx" } }
 }
 
+-- Emmet
+--
+local emmet_options = {
+    filetypes = {
+        "html",
+        "css",
+        "javascript",
+        "javascriptreact",
+        "typescript",
+        "typescriptreact",
+        "svelte",
+        "xml",
+    },
+}
+
+require("lvim.lsp.manager").setup("emmet_ls", emmet_options)
+
+
+vim.g.go_addtags_transform = "camelcase"
 
 -- turns off LSP semantic tokens by default for nord - treesitter ...
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        client.server_capabilities.semanticTokensProvider = nil
-    end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--     callback = function(args)
+--         local client = vim.lsp.get_client_by_id(args.data.client_id)
+--         client.server_capabilities.semanticTokensProvider = nil
+--     end,
+-- })
 
 require("todo-comments").setup({})
 
@@ -132,4 +132,40 @@ vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
 
 
-lvim.colorscheme = "nord"
+lvim.colorscheme = "rose-pine-moon"
+
+lvim.builtin.alpha.dashboard.section.header.val = {
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⡿⣿⢏⡼⠀⢀⣾⢃⣼⠏⢠⡾⠃⣠⠏⠀⠁⣀⠀⡁⠀⠀⣠⡈⠉⡉⠉⢻⡄⠀⠀⠈⠈⢄⠐⣦",
+    "⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣯⣾⠃⣼⢁⢠⡿⣡⣾⠃⣴⣿⠁⣠⡟⠀⠄⠡⢀⠈⠀⣠⣱⣿⠇⠡⠄⠀⠀⢿⡆⠈⠄⡁⠈⣳⣽",
+    "⠄⠁⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣯⣿⠇⣼⡇⠀⣿⢓⣿⠧⣼⣻⠇⢠⣿⠁⠀⠌⠰⠀⢀⣴⣿⡟⣹⠀⠐⡌⠄⠸⡌⣿⠀⠀⠄⠄⢹⣿",
+    "⠄⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣟⣟⢨⣿⠀⣼⣏⣾⡟⣰⣟⣿⢣⣿⡟⠀⠈⢀⢀⣄⣾⣿⡟⣇⣿⣀⡐⣈⡀⠐⣿⢹⡇⠀⠁⠂⢸⣿",
+    "⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣻⣹⡇⣯⣿⠀⣿⣿⣿⣿⣿⣼⣧⡟⣽⡃⠀⢈⠈⣭⣿⡿⠃⠉⣿⣿⡍⢉⡏⠙⠻⣿⣼⡷⣬⣶⢷⣿⡟",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢽⣸⡗⣯⣿⢘⣿⢼⣿⣿⣿⣿⣿⢠⣿⠀⠀⣠⡾⣻⣟⣡⣄⡀⣽⡏⣧⢸⡇⠂⠱⢺⣿⣗⡻⣽⣾⣿⠘",
+    "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢼⡿⣷⣹⣿⡞⡿⣸⣿⣿⣿⣿⡏⢸⡿⠀⣰⡿⢣⡿⢻⣿⣿⣷⣿⣥⣿⣾⢡⠀⠐⣾⣿⡝⣧⣿⣿⢹⡀",
+    "⠀⠀⠀⠀⢀⣠⣄⣀⣀⣀⣠⣸⣷⣿⣿⣿⢿⣇⠹⣟⡹⠿⡟⠀⠀⣿⣼⠏⢠⠋⠀⣿⣿⣿⣿⣿⣿⠿⣿⡇⠀⢨⣿⣿⡽⣟⡿⠁⢸⠅",
+    "⠀⠀⠀⢰⣿⡟⠿⠿⢯⡿⣽⣿⣻⣿⣯⣿⣌⣷⠀⠉⠛⠁⠀⠀⢨⡿⠁⠀⠀⠀⠀⣿⠟⣿⢿⣿⠟⣰⡟⠀⠠⣼⣿⢯⣽⠟⣠⠀⢸⡇",
+    "⠀⠀⢠⣿⣻⠃⢠⣄⣤⣔⣃⡀⠉⠉⠙⠛⠻⠾⢧⣤⣤⣤⣤⣀⣈⠀⠀⠀⠀⠀⠀⠙⠿⠶⠞⠁⣰⡿⠁⢀⣽⢻⣿⠟⠁⠀⢹⡄⠐⣿",
+    "⡄⠀⣾⣧⡟⠀⣿⡏⠉⠉⠛⠁⢈⣿⠃⠀⠀⠀⢠⣦⡀⢠⣌⡉⠙⠛⠛⠷⠶⠶⠴⣶⣤⣦⣰⣾⡟⠁⢠⡾⢫⣿⡟⠀⠰⠈⢌⣧⠀⢿",
+    "⡁⠀⣿⣽⠃⠀⠻⣿⣦⡀⠐⡁⢸⣟⠀⠀⠀⠀⢸⣿⡉⢸⡿⠁⠀⠁⠈⣷⡦⣷⣤⣤⣄⣠⡉⠉⠙⠛⣿⡿⣟⣿⣇⠀⠰⢈⠀⢿⡀⢈",
+    "⢶⣴⣿⡟⠀⠀⠀⠀⠛⢿⣦⢀⣿⡟⠶⠶⢶⣦⣿⡇⡘⣿⡇⠀⠀⠀⢀⣿⠁⠀⠉⢹⣿⠛⠛⠋⠀⢠⣿⣗⡣⢿⣿⠀⠐⠠⠀⢸⣷⡀",
+    "⣿⢿⣷⡇⠰⢷⣤⣤⣤⣿⢿⣸⣿⠁⠀⠀⠀⢠⣿⢱⢱⣿⠁⠀⠀⠀⣸⣿⠀⠈⡄⣿⡟⠀⠀⠀⠀⣸⣿⣶⢹⣻⣿⡄⠀⡁⠂⠈⣿⣷",
+    "⣿⠉⢥⡂⠀⠀⠈⠉⠉⠀⠀⠛⠛⠀⠀⠀⠀⢼⣿⠘⠘⣿⣄⠀⠀⢢⣿⡇⠀⠐⢆⣿⠃⠀⠀⠀⢀⣿⣗⣻⣧⢚⣿⣧⠀⠐⠠⠄⢻⣿",
+    "⣿⡀⡸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠛⠿⠛⠋⠀⠀⠀⣸⣿⠀⠀⠐⡏⠹⣿⣧⢏⣷⢫⡜⣿⡆⠀⠃⠌⠘⣟",
+    "⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⢰⣶⠆⠀⠀⠀⣀⣀⠀⣁⠀⠀⠂⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠸⣿⠉⠁⢻⣎⢿⡧⡽⣹⣿⡄⠉⠄⠈⢷",
+    "⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡟⠀⠀⠀⢀⣿⡏⠂⣿⡿⠿⣶⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⡶⠛⠙⣿⡞⣿⡱⣧⣛⣿⡄⠀⠈⠈",
+    "⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠀⢸⣿⠃⢰⣿⠃⢀⡝⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠀⠄⣿⣳⢻⣧⡿⡹⡽⣷⡄⠀⠠",
+    "⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠀⠀⠀⠀⣸⡟⠀⣼⣯⣤⣼⣽⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣯⠘⠀⣿⣧⢻⡷⣿⡱⢏⡿⣿⣄⠀",
+    "⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣦⣀⣀⣠⣿⠃⢡⣿⡇⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⢿⣄⣁⣿⣏⣯⣿⣿⠱⣏⣿⢿⣿⣦",
+    "⣶⣶⢦⡤⣤⣄⣀⣀⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠸⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⣬⠿⡻⣍⠯⣹⣿⢇⡿⡜⣿⡇⣿⣿",
+    "⠋⠙⠛⠛⢲⠾⢯⣽⣟⣟⣶⡶⢦⣤⣤⣄⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣍⡲⣍⠳⢎⡳⣽⡟⢮⡳⡝⣿⡇⢸⣿",
+    "⠀⠀⠀⢀⣀⢀⡉⣴⣮⣉⣉⣹⣟⣿⡯⣭⣿⣻⣯⡿⣟⢳⡶⢤⡤⣤⣀⣀⣀⠀⠀⠀⠀⢰⣟⣛⣛⢳⡹⣎⣶⣿⣹⢣⣛⠵⣻⠆⠸⣿",
+}
+
+lvim.builtin.alpha.dashboard.section.footer.val = {
+    "Don't worry"
+}
+
+lvim.builtin.alpha.dashboard.section.buttons.entries.insert = {
+    "Shift + x",
+    "󰮢  rm -rf /",
+}
