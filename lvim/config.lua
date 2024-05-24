de8fdc8bb5dea3de1d06a7a3eb47ad39ab228f3b
Wralith <wralithdev@gmail.com>
2024-05-24 23:17:46 +0300

    update copilot, templ lsp

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,4 +1,4 @@
-lvim.log.level = "warn"
+lvim.log.level = "info"
 lvim.builtin.lualine.sections.lualine_a = { "mode" }
 lvim.builtin.lualine.options.component_separators = { left = "", right = "" }
 lvim.builtin.alpha.active = true
@@ -19,6 +19,8 @@ lvim.keys.normal_mode["K"] = ":lua vim.lsp.nuf.hover()<CR>"
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 90
 
+vim.opt.shell = "/bin/sh"
+
 lvim.builtin.treesitter.highlight.enable = true
 
 -- hover remove borders
@@ -27,10 +29,14 @@ lvim.builtin.cmp.window = {
     },
 }
 
+lvim.builtin.project.patterns = { ".git", "package.json", "go.mod" }
+
 -- plugins
 lvim.plugins = {
     -- Colorschemes
+    -- { "williamboman/mason-lspconfig.nvim"},
     { "shaunsingh/nord.nvim" },
+    { "rebelot/kanagawa.nvim" },
     { "catppuccin/nvim",                name = "catpuccin" },
     { "rose-pine/neovim",               name = "rose-pine" },
     { "neanias/everforest-nvim" },
@@ -66,6 +72,22 @@ lvim.plugins = {
     { "theHamsta/nvim-dap-virtual-text" },
     { "nvim-telescope/telescope-dap.nvim" },
 
+    -- MOST IMPORTANT PLUGIN EVER
+    {
+        "giusgad/pets.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
+        config = function()
+            require("pets").setup({
+                row = 4,
+                popoup = {
+                    width = "10%",
+                    avoid_statusline = true
+                }
+
+            })
+        end
+    },
+
     -- Utilities
     {
         "phaazon/hop.nvim",
@@ -77,18 +99,34 @@ lvim.plugins = {
             vim.api.nvim_set_keymap("n", "S", ":HopPattern<CR>", {})
         end
     },
-    {
-        "github/copilot.vim",
-        event = "VeryLazy",
-        config = function()
-            -- copilot assume mapped
-            vim.g.copilot_assume_mapped = true
-            vim.g.copilot_no_tab_map = true
-            vim.g.copilot_filetypes = { yaml = true, yml = true }
-        end,
-    },
+    -- {
+    --     "github/copilot.vim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --         -- copilot assume mapped
+    --         vim.g.copilot_assume_mapped = true
+    --         vim.g.copilot_no_tab_map = true
+    --         vim.g.copilot_filetypes = { yaml = true, yml = true }
+    --         -- require("copilot").setup({
+    --         --     keys: {
+    --         --         ac
+    --         --     }
+    --         -- })
 
+    --     end,
+    -- },
     -- Kubernetes
+    {
+        "zbirenbaum/copilot.lua",
+        config = function()
+            require("copilot").setup({
+                suggestion = {
+                    enabled = true,
+                    auto_trigger = true,
+                },
+            })
+        end
+    },
     {
         "someone-stole-my-name/yaml-companion.nvim",
         config = function()
@@ -115,6 +153,21 @@ lvim.plugins = {
     }
 }
 
+-- copilot
+--
+-- table.insert(lvim.plugins, {
+--     "zbirenbaum/copilot-cmp",
+--     event = "InsertEnter",
+--     dependencies = { "zbirenbaum/copilot.lua" },
+--     config = function()
+--         vim.defer_fn(function()
+--             require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+--             require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+--         end, 100)
+--     end,
+-- })
+
+
 -- dap
 --
 local dap_ok, dapgo = pcall(require, "dap-go")
@@ -131,7 +184,7 @@ local formatters = require("lvim.lsp.null-ls.formatters")
 local code_actions = require("lvim.lsp.null-ls.code_actions")
 
 linters.setup {
-    { command = "eslint_d", filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } }
+    { command = "eslint", filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } }
 }
 formatters.setup {
     {
@@ -142,8 +195,13 @@ formatters.setup {
 formatters.setup {
     { command = "shfmt", filetypes = { "sh" } }
 }
+
+formatters.setup({
+    { command = "black", filetypes = { "python" } }
+})
+
 code_actions.setup {
-    { command = "eslint_d", filetypes = { "typescript", "typescriptreact", "js", "jsx" } }
+    { command = "eslint", filetypes = { "typescript", "typescriptreact", "js", "jsx" } }
 }
 
 -- Emmet
@@ -158,11 +216,16 @@ local emmet_options = {
         "typescriptreact",
         "svelte",
         "xml",
+        "templ"
     },
 }
 
 -- Tailwind for CVA
 require 'lspconfig'.tailwindcss.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+    init_options = { userLanguages = { templ = "html" } },
     settings = {
         tailwindCSS = {
             experimental = {
@@ -173,6 +236,18 @@ require 'lspconfig'.tailwindcss.setup({
             },
         },
     },
+})
+
+require 'lspconfig'.html.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "templ" },
+})
+
+require 'lspconfig'.htmx.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "templ" },
 })
 
 require("lvim.lsp.manager").setup("emmet_ls", emmet_options)
@@ -205,12 +280,14 @@ require("lvim.lsp.manager").setup("cssls", {
 vim.g.go_addtags_transform = "camelcase"
 
 -- turns off LSP semantic tokens by default for nord - treesitter ...
--- vim.api.nvim_create_autocmd("LspAttach", {
---     callback = function(args)
---         local client = vim.lsp.get_client_by_id(args.data.client_id)
---         client.server_capabilities.semanticTokensProvider = nil
---     end,
--- })
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        client.server_capabilities.semanticTokensProvider = nil
+    end,
+})
+--
+vim.filetype.add({ extension = { templ = "templ" } })
 
 require("todo-comments").setup({})
 
@@ -225,10 +302,10 @@ vim.opt.spell = true
 vim.opt.spelllang = { "en_us" }
 
 
-lvim.colorscheme = "everforest"
-require("everforest").setup({
-    background = "hard"
-})
+lvim.colorscheme = "kanagawa-dragon"
+-- require("everforest").setup({
+--     background = "hard"
+-- })
 
 lvim.builtin.alpha.dashboard.section.header.val = {
     " ██░ ██  ▄▄▄     ▓██   ██▓ ▄▄▄     ▄▄▄█████▓    ▄▄▄▄    ██▓▄▄▄█████▓▄▄▄█████▓ ██▓",
@@ -250,4 +327,11 @@ lvim.builtin.alpha.dashboard.section.footer.val = {
 lvim.builtin.alpha.dashboard.section.buttons.entries.insert = {
     "Shift + x",
     "󰮢  rm -rf /",
+}
+
+local lspconfig = require("lspconfig")
+
+
+formatters.setup {
+    { command = "templ", filetypes = { "templ" } }
 }
