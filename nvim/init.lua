1bc5a12279c714e4e74911880c77a9204f135e45
Wralith <wralithdev@gmail.com>
2025-02-03 00:45:20 +0300

    new shiny things

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,19 @@
-require('plug')
-require('keymap')
-require('options')
-require('config.plug')
-require("themes.tokyonight")
-require('themes.everforest')
-require('themes.lualine.everforest')
-require('config.lsp')
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd[[colorscheme catppuccin-frappe]]
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require "lazy_setup"
+require "polish"
---
+++ nvim/lua/community.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.motion.hop-nvim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.completion.copilot-lua" },
+}
